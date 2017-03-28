4d7a7b6ed4b5a314026da71d1443dd18f0bd87e3
Andrei Lapets <a@lapets.io>
2017-03-27 22:56:34 -0400

    Update code to simulate student locations.

--- generate_student_data.py
+++ generate_student_data.py
@@ -1,15 +1,18 @@
-import json, os
-import shapefile
-import pyproj
+import os
+import random
 import zipfile
+import json
+import geojson
+import shapefile # pyshp library
+import pyproj
+from tqdm import tqdm
+import geoleaflet
 
-
-# Determine if a point is inside a given polygon or not
-# Polygon is a list of (x,y) pairs. This function
-# returns True or False.  The algorithm is called
-# the "Ray Casting Method".
-# Taken from geospatialpython.com
-def point_in_poly(x,y,poly):
+def point_in_poly(x, y, poly):
+    """
+    Determine whether a point is inside a given polygon (list of (x,y) pairs).
+    Returns True or False. Uses ray casting (source: geospatialpython.com).
+    """
     n = len(poly)
     inside = False
 
@@ -24,61 +27,91 @@ def point_in_poly(x,y,poly):
                     if p1x == p2x or x <= xints:
                         inside = not inside
         p1x,p1y = p2x,p2y
-
     return inside
 
-def load_zipcode_data():
+def extract_zipcode_data():
     if not os.path.isdir('zipcodes_nt'):
         with zipfile.ZipFile('zipcodes_nt.zip') as f:
             f.extractall("./zipcodes_nt")
 
+def properties_by_zipcode(file_prefix):
+    """
+    Build a JSON file grouping all properties by zip code.
+    """
+    sf = shapefile.Reader('zipcodes_nt/ZIPCODES_NT_POLY')
+    reverse_coordinate_projection = pyproj.Proj(proj = 'lcc', datum = 'NAD83',
+                                    lat_1 = 41.71666666666667, lat_2 = 42.68333333333333,
+                                    lat_0 = 41.0, lon_0 = -71.5,
+                                    x_0 = 200000.0, y_0 = 750000.0)
 
-load_zipcode_data()
-sf = shapefile.Reader('zipcodes_nt/ZIPCODES_NT_POLY')
-reverse_coordinate_projection = pyproj.Proj(proj = 'lcc', datum = 'NAD83',
-                                lat_1 = 41.71666666666667, lat_2 = 42.68333333333333,
-                                lat_0 = 41.0, lon_0 = -71.5,
-                                x_0 = 200000.0, y_0 = 750000.0)
+    ZIPFIELD = 0 # Constant for the zipcode field.
 
-# Define a constant for the zipcode field
-ZIPFIELD = 0
+    # Load all residences in Boston.
+    properties = json.load(open('properties.geojson', 'r'))
+    residences = [p for p in properties.items() if p[1]['properties']['type'] == 'Residential']
 
-# Load all residences in Boston
-properties = json.load(open('properties.geojson', 'r'))
-residences = [p for p in properties.items() if p[1]['properties']['type'] == 'Residential']
+    # Convert shapefile to dictionary one time.
+    # The shapefile actually does not contain unique zipcode data so we have to make sure to have multipolygons.
+    # For now, just ignore the list.
+    blacklist = ['01434', '01082', '02532', '01002', '01039', '01050', '02467', '01096', '01026', '01011', '01247', '01010', '01235', '01008']
 
-# Convert shapefile to dictionary one time
-# The shapefile actually does not contain unique zipcode data
-# so we have to make sure to have multipolygons
-# For now, just ignore the list:
-blacklist = ['01434', '01082', '02532', '01002', '01039', '01050', '02467', '01096', '01026', '01011', '01247', '01010', '01235', '01008']
+    zipcode_polygons = {}
+    for sr in sf.iterShapeRecords():
+        zc = sr.record[ZIPFIELD] # zip code
+        if zc in blacklist:
+            continue # FIX THIS
+        polygon = []
+        for x,y in sr.shape.points:
+            lng, lat = reverse_coordinate_projection(x, y, inverse=True)
+            polygon.append((lat, lng))
 
-zipcode_polygons = {}
-for sr in sf.iterShapeRecords():
-    zc = sr.record[ZIPFIELD] # zip code
-    if zc in blacklist: continue # FIX THIS
+        zipcode_polygons[zc] = polygon
 
-    polygon = []
+    # Map a list of residences to their zip codes.
+    zipcodes = {zc:[] for zc in zipcode_polygons}
+    for k, residence in tqdm(residences):
+        res_lat, res_lng = residence['geometry']['coordinates']
+        for zc, polygon in zipcode_polygons.items():
+            if point_in_poly(res_lat, res_lng, polygon):
+                zipcodes[zc].append((k, residence))
+                break
 
-    for x, y in sr.shape.points:
-        lng, lat = reverse_coordinate_projection(x, y, inverse=True)
-        polygon.append((lat, lng))
+    with open(file_prefix + '.json', 'w') as f:
+        f.write(json.dumps(zipcodes))
 
-    
-    zipcode_polygons[zc] = polygon
+def percentages_csv_to_json(file_prefix):
+    rows = open(file_prefix + '.csv', 'r').read().split("\n")
+    fields = rows[0].split("\t")
+    rows = [list(zip(fields, row.split("\t"))) for row in tqdm(rows[1:])]
+    zip_to_percentages = {}
+    for r in rows:
+        zip_to_percentages[r[0][1]] = {
+            'total': int(r[3][1]),
+            'schools': dict([(f,float(v)) for (f,v) in r[4:] if float(v) > 0])
+          }
+    txt = json.dumps(zip_to_percentages, indent=2)
+    open(file_prefix + '.json', 'w').write(txt)
 
-# map a list of residences to its zip code
-zipcodes = {zc:[] for zc in zipcode_polygons}
-count = 0
-for k, residence in residences:
-    if count % 1000 == 0:
-        print(count)
-    res_lat, res_lng = residence['geometry']['coordinates']
-    for zc, polygon in zipcode_polygons.items():
-        if point_in_poly(res_lat, res_lng, polygon):
-            zipcodes[zc].append((k, residence))
-            break
-    count += 1
+def students_simulate(file_prefix_properties, file_prefix_percentages, file_prefix_students):
+    properties = json.loads(open(file_prefix_properties + '.json', 'r').read())
+    percentages = json.loads(open(file_prefix_percentages + '.json', 'r').read())
+    features = []
+    for zip in percentages.keys() & properties.keys():
+        for location in tqdm(random.sample(properties[zip], percentages[zip]['total'])):
+            geometry = location[1]['geometry']
+            (x, y) = (geometry['coordinates'][0], geometry['coordinates'][1])
+            geometry['coordinates'][1] = x
+            geometry['coordinates'][0] = y
+            props = {}
+            features.append(geojson.Feature(geometry=geometry, properties=props))
+    open(file_prefix_students + '.geojson', 'w').write(geojson.dumps(geojson.FeatureCollection(features), indent=2))
+    return geojson.FeatureCollection(features)
 
-with open('zipcodes.json', 'w') as f:
-    f.write(json.dumps(zipcodes))
+def main():
+    extract_zipcode_data()
+    properties_by_zipcode('properties-by-zipcode')
+    percentages_csv_to_json('student-zip-school-percentages')
+    students = students_simulate('properties-by-zipcode', 'student-zip-school-percentages', 'students')
+    open('students.html', 'w').write(geoleaflet.html(students))
+
+## eof
