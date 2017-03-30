077b343e5a17860c2d3ce43838452f3e495c9001
Andrei Lapets <a@lapets.io>
2017-03-30 00:10:36 -0400

    Add manually cleaned list of schools.

--- generate_student_data.py
+++ generate_student_data.py
@@ -64,7 +64,6 @@ def properties_by_zipcode(file_prefix):
         for x,y in sr.shape.points:
             lng, lat = reverse_coordinate_projection(x, y, inverse=True)
             polygon.append((lat, lng))
-
         zipcode_polygons[zc] = polygon
 
     # Map a list of residences to their zip codes.
@@ -93,23 +92,19 @@ def percentages_csv_to_json(file_prefix):
     open(file_prefix + '.json', 'w').write(txt)
 
 def zip_to_school_to_location(file_prefix):
-    bps_to_cob = json.loads(open('school-names-bps-to-cob.json', 'r').read())
-    cob_to_bps = {v:k for k,v in bps_to_cob.items()}
     rows = open(file_prefix + '.csv', 'r').read().split("\n")
-    fields = rows[0].split(",")
-    rows = [dict(zip(fields, row.split(","))) for row in rows[1:]]
-    zips = {row['ZIPCODE'] for row in rows[1:] if 'SCH_LABEL' in row}
-    zip_to_cob_name_to_loc = {
-        z:{
-            cob_to_bps[r['SCH_LABEL'].strip()]: (float(r['X']), float(r['Y'])) 
+    fields = rows[0].split("\t")
+    rows = [dict(zip(fields, row.split("\t"))) for row in rows[1:]]
+    zips = {row['zip'] for row in rows[1:]}
+    zip_to_name_to_loc = {
+        zip:{
+            r['name'].strip(): (float(r['longitude']), float(r['latitude'])) 
             for r in rows[1:] 
-            if 'SCH_LABEL' in r 
-                and r['SCH_LABEL'].strip() in cob_to_bps 
-                and z == r['ZIPCODE']
+            if zip == r['zip']
           }
-        for z in zips
+        for zip in zips
       }
-    return zip_to_cob_name_to_loc
+    return zip_to_name_to_loc
 
 def students_simulate(file_prefix_properties, file_prefix_percentages, file_prefix_students):
     props = json.loads(open(file_prefix_properties + '.json', 'r').read())
@@ -122,11 +117,12 @@ def students_simulate(file_prefix_properties, file_prefix_percentages, file_pref
             for (school, fraction) in tqdm(percentages[zip]['schools'].items()):
                 if school in schools_to_location:
                     school_loc = schools_to_location[school]
-                    for student in range(int(0.5 * fraction * percentages[zip]['total'])):
-                        locations = list(sorted([(geopy.distance.vincenty(tuple(reversed(prop[1]['geometry']['coordinates'])), school_loc).miles, prop) for prop in random.sample(props[zip], 50)]))
+                    for student in range(int(1.0 * fraction * percentages[zip]['total'])):
+                        r = random.randint(1,5)
+                        locations = list(sorted([(geopy.distance.vincenty(tuple(reversed(prop[1]['geometry']['coordinates'])), school_loc).miles, prop) for prop in random.sample(props[zip], r)]))
                         location = locations[0][1]
-                        start = tuple(reversed(location[1]['geometry']['coordinates']))
                         end = school_loc
+                        start = tuple(reversed(location[1]['geometry']['coordinates']))
                         geometry = geojson.Point(start)
                         geometry = geojson.LineString([start, end])
                         properties = {'length': geopy.distance.vincenty(start, end).miles}
@@ -134,7 +130,8 @@ def students_simulate(file_prefix_properties, file_prefix_percentages, file_pref
         else:
             pass #print(zip)
     open(file_prefix_students + '.geojson', 'w').write(geojson.dumps(geojson.FeatureCollection(features), indent=2))
-    return geojson.FeatureCollection(list(reversed(sorted(features, key=lambda f: f['properties']['length']))))
+    features = list(reversed(sorted(features, key=lambda f: f['properties']['length'])))
+    return geojson.FeatureCollection(features)
 
 def main():
     #extract_zipcode_data()
