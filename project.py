c1b37995268f2043bda0a9b075a7a83856b47cda
Adrian Law <adrian@Adrians-MacBook-Pro.local>
2017-04-12 23:17:04 -0400

    runtime error divide by zero

--- project.py
+++ project.py
@@ -20,7 +20,7 @@ def project(p1, l1, l2):
 
     line = l2 - l1
     vec = p1 - l1
-    return vec.dot(line) / line.dot(line) * line  # Projects vec onto line
+    return vec.dot(line) / (line.dot(line) * line)  # Projects vec onto line
 
 def normal(p1, l1, l2):
     p1 = np.array(p1)
@@ -56,47 +56,47 @@ def rTreeify(points):
 
     return tree, tree_keys
 
-def coordinate_distance(v,w,p):
-    '''find distance of point to line segment'''
-    seglen_squared = (v[0] - w[0])**2 + (v[1] - w[1])**2
-    #t = max(0, min(1, dot(p - v, w - v) / l2))
-    t =  max(0, min(1, ((p[0]-v[0])*(w[0]-v[0]) + (p[1]-v[1])*(w[1]-v[1]))/seglen_squared ))
-    #projection = v + t * (w - v)
-    projection = (t*(w[0] - v[0])+v[0],t*(w[1] - v[1])+v[1])
-    return ((p[0] - projection[0])**2 + (p[1] - projection[1])**2 )**0.5
+# def coordinate_distance(v,w,p):
+#     '''find distance of point to line segment'''
+#     seglen_squared = (v[0] - w[0])**2 + (v[1] - w[1])**2
+#     #t = max(0, min(1, dot(p - v, w - v) / l2))
+#     t =  max(0, min(1, ((p[0]-v[0])*(w[0]-v[0]) + (p[1]-v[1])*(w[1]-v[1]))/seglen_squared ))
+#     #projection = v + t * (w - v)
+#     projection = (t*(w[0] - v[0])+v[0],t*(w[1] - v[1])+v[1])
+#     return ((p[0] - projection[0])**2 + (p[1] - projection[1])**2 )**0.5
 
-def points_linestrings_coverage(linestring,points_tree,points_tree_keys,points_dict, r):
-    '''Using route from dictionary and the points tree, return list of points_tree_keys that are within distance r of the route'''
+# def points_linestrings_coverage(linestring,points_tree,points_tree_keys,points_dict, r):
+#     '''Using route from dictionary and the points tree, return list of points_tree_keys that are within distance r of the route'''
 
-    multipoint = linestring['geometry']['coordinates']
+#     multipoint = linestring['geometry']['coordinates']
     
-    intersecting_points = set()
+#     intersecting_points = set()
 
-    #for each pair of points, find points within the box of endpoints extended by r
-    for j in range(1,len(multipoint)):
-        #coordinates of endpoints
-        yi, xi = multipoint[j-1]
-        yj, xj = multipoint[j]
+#     #for each pair of points, find points within the box of endpoints extended by r
+#     for j in range(1,len(multipoint)):
+#         #coordinates of endpoints
+#         yi, xi = multipoint[j-1]
+#         yj, xj = multipoint[j]
 
-        result_set = set()
-        x_min = min(xi, xj) - r
-        x_max = max(xi, xj) + r
-        y_min = min(yi, yj) - r
-        y_max = max(yi, yj) + r
+#         result_set = set()
+#         x_min = min(xi, xj) - r
+#         x_max = max(xi, xj) + r
+#         y_min = min(yi, yj) - r
+#         y_max = max(yi, yj) + r
         
-        for i in list(points_tree.intersection((x_min,y_min,x_max,y_max))):
-            result_set.add(i)
+#         for i in list(points_tree.intersection((x_min,y_min,x_max,y_max))):
+#             result_set.add(i)
 
-        #for each point in the intersecting set, determine if it's within distance r of the line:
+#         #for each point in the intersecting set, determine if it's within distance r of the line:
         
-        for i in result_set:
-            key = points_tree_keys[str(i)]
-            coor = points_dict[key]['geometry']['coordinates']
+#         for i in result_set:
+#             key = points_tree_keys[str(i)]
+#             coor = points_dict[key]['geometry']['coordinates']
 
-            if coordinateDistance((xi, yi), (xj, yj), (coor[1], coor[0])) <= r:
-                intersecting_points.add(i)
+#             if coordinateDistance((xi, yi), (xj, yj), (coor[1], coor[0])) <= r:
+#                 intersecting_points.add(i)
 
-    return intersecting_points
+#     return intersecting_points
 
 # UNFINISHED
 def project_points_to_linestrings(points, linestrings):
@@ -123,25 +123,29 @@ def project_points_to_linestrings(points, linestrings):
 
 def load_road_segments(fname):
     linestrings = geojson.loads(open(fname, 'r').read())
-    linestrings.features = [seg for seg in tqdm(linestrings.features) if seg.type=='Feature']
+    linestrings.features = [seg for seg in linestrings.features if seg.type=='Feature']
     return linestrings
 
 def generate_student_stops(student_points, numStops=5000, loadFrom=None):
+
+    # get means from picle or generate
     if loadFrom:
         means = pickle.load(open(loadFrom, 'rb'))
+
     else:
-        #load student coordinates from students datafile to list of coordinates
+        # load student coordinates from students datafile to list of coordinates
         points = [student_points['features'][i]['geometry']['coordinates'][0] for i in range(len(student_points['features']))]
     
         #generate means
         kmeans = KMeans(n_clusters=numStops, random_state=0)
         means = kmeans.fit(points).cluster_centers_
 
-    #to do: assign each mean to the closest line segment
-    #       project mean to the line segment
-    #       return projected points
+    # get linestrings from roadsegments
     linestrings = load_road_segments('example_extract_missing.geojson')
+
     #return means, linestrings
     return project_points_to_linestrings(means, linestrings)
 
 stops = generate_student_stops([], loadFrom='kmeans')
+
+
