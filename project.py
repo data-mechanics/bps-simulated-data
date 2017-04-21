149a35983d883637cb8397a34ece4aa7f906a486
Tyrone Hou <tyroneh@bu.edu>
2017-04-21 16:31:40 +0000

    Changed projection method to map to corners for far points and generated new stops

--- project.py
+++ project.py
@@ -21,16 +21,15 @@ def project(p1, l1, l2):
     line = l2 - l1
     vec = p1 - l1
     #print(l1, l2, vec, line, line.dot(line), flush=True)
-    return (vec.dot(line) / line.dot(line)) * line  # Projects vec onto line
+    return l1 + (vec.dot(line) / line.dot(line)) * line  # Projects vec onto line
 
 def normal(p1, l1, l2):
     p1 = np.array(p1)
     l1 = np.array(l1)
     l2 = np.array(l2)
 
-    proj = project(p1, l1, l2)
-    norm = l1 + proj - p1
-    return norm.dot(norm)
+    proj = project_point_to_segment(p1, l1, l2)
+    return proj - p1
 
 def project_point_to_segment(p1, l1, l2):
     p1 = np.array(p1)
@@ -38,12 +37,16 @@ def project_point_to_segment(p1, l1, l2):
     l2 = np.array(l2)
 
     proj = project(p1, l1, l2)
+    v1 = proj - l1
+    v2 = proj - l2
 
-    # x on segment
-    if (proj-l1).dot(proj-l2) <= 0:
-        return x
-    else: # x not on segment
-        return min(p1-l1, p1-l2)
+    # v1 and v2 face in opposite directions if the dot product is negative 
+    if v1.dot(v2) <= 0:
+        return proj
+    elif np.dot(v1, v1) <= np.dot(v2, v2): # proj not on segment
+        return l1
+    else: # distance from point to l2 is smaller
+        return l2
 
 def linestrings_to_adj_list(linestrings):
     '''converts a list of linestrings into an adjacency list of edges'''
@@ -70,72 +73,29 @@ def rTreeify(points):
 
     return tree, tree_keys
 
-# def coordinate_distance(v,w,p):
-#     '''find distance of point to line segment'''
-#     seglen_squared = (v[0] - w[0])**2 + (v[1] - w[1])**2
-#     #t = max(0, min(1, dot(p - v, w - v) / l2))
-#     t =  max(0, min(1, ((p[0]-v[0])*(w[0]-v[0]) + (p[1]-v[1])*(w[1]-v[1]))/seglen_squared ))
-#     #projection = v + t * (w - v)
-#     projection = (t*(w[0] - v[0])+v[0],t*(w[1] - v[1])+v[1])
-#     return ((p[0] - projection[0])**2 + (p[1] - projection[1])**2 )**0.5
-
-# def points_linestrings_coverage(linestring,points_tree,points_tree_keys,points_dict, r):
-#     '''Using route from dictionary and the points tree, return list of points_tree_keys that are within distance r of the route'''
-
-#     multipoint = linestring['geometry']['coordinates']
-    
-#     intersecting_points = set()
-
-#     #for each pair of points, find points within the box of endpoints extended by r
-#     for j in range(1,len(multipoint)):
-#         #coordinates of endpoints
-#         yi, xi = multipoint[j-1]
-#         yj, xj = multipoint[j]
-
-#         result_set = set()
-#         x_min = min(xi, xj) - r
-#         x_max = max(xi, xj) + r
-#         y_min = min(yi, yj) - r
-#         y_max = max(yi, yj) + r
-        
-#         for i in list(points_tree.intersection((x_min,y_min,x_max,y_max))):
-#             result_set.add(i)
-
-#         #for each point in the intersecting set, determine if it's within distance r of the line:
-        
-#         for i in result_set:
-#             key = points_tree_keys[str(i)]
-#             coor = points_dict[key]['geometry']['coordinates']
-
-#             if coordinateDistance((xi, yi), (xj, yj), (coor[1], coor[0])) <= r:
-#                 intersecting_points.add(i)
-
-#     return intersecting_points
-
 # UNFINISHED
 def project_points_to_linestrings(points, linestrings):
     # Todo: Implement rtrees to find line points within certain distance
 
     projections = []
     for x,y in tqdm(points):
-        p = [y, x]
+        p = np.array([x, y])
         lstr_copy = deepcopy(linestrings)
-        lstr_copy = geoql.features_keep_within_radius(lstr_copy, p, 0.5, 'miles')
+        lstr_copy = geoql.features_keep_within_radius(lstr_copy, [y,x], 0.5, 'miles')
         min_proj = (10000, [0,0])
         for lstr in lstr_copy.features:
             segments = lstr.geometry.coordinates
             for i in range(len(segments)-1):
-                #if np.linalg.norm(np.array(segments[i+1]) - np.array(segments[i])) == 0:
-
+                if np.linalg.norm(np.array(segments[i+1]) - np.array(segments[i])) == 0:
+                    continue
                 norm = normal(p, segments[i], segments[i+1])
-                          
-                if norm < min_proj[0]:
-                        proj = project(p, segments[i], segments[i+1])
-                        min_proj = (norm, proj)
-
+                dist = norm.dot(norm)
+                if dist < min_proj[0]:
+                    proj = project_point_to_segment(p, segments[i], segments[i+1])
+                    min_proj = [dist, proj]
         projections.append(min_proj)
 
-    return [list(p[1]) for p in projections]
+    return [p[1] for p in projections]
 
 def load_road_segments(fname):
     linestrings = geojson.loads(open(fname, 'r').read())
@@ -160,10 +120,8 @@ def generate_student_stops(student_points, numStops=5000, loadFrom=None):
     linestrings = load_road_segments('example_extract_missing.geojson')
 
     #return means, linestrings
-    return project_points_to_linestrings(means, linestrings)
+    return means, project_points_to_linestrings(means, linestrings)
 
-stops = generate_student_stops([], loadFrom='kmeans')
+points, stops = generate_student_stops([], loadFrom='kmeans')
 with open('stops', 'wb') as f:
     f.write(pickle.dumps(stops))
-#with open('student_stops', '')
-
