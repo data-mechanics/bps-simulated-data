f9d0a03cfa6b7730eb9a3bc2609b224db24d11a1
Andrei Lapets <a@lapets.io>
2017-06-05 04:39:45 -0400

    Fix route assignment loop bug; build R-tree in loop; assign buses to students.

--- generate-route-data.py
+++ generate-route-data.py
@@ -50,40 +50,70 @@ class Route():
 
 def stops_to_dict(file_json):
     stops = json.load(open(file_json, 'r'))
-    return {tuple(map(float, sch.split(','))):{tuple(map(float, stp.split(','))):stops[sch][stp] for stp in stops[sch]} for sch in stops}
+    school_stop_to_load = {}
+    for [sch, stp, load] in stops:
+        (sch, stp) = (tuple(sch), tuple(stp))
+        if sch not in school_stop_to_load:
+            school_stop_to_load[sch] = {}
+        if stp not in school_stop_to_load[sch]:
+            school_stop_to_load[sch][stp] = load
+    return school_stop_to_load
 
-def school_to_stops_rtree(stops):
+def school_to_stops(stops):
     mapping = {}
     for school in stops:
-        school_stops = list(stops[school].items())
-        school_rtree = rtree.index.Index()
-        for (i, ((lon, lat), count)) in enumerate(school_stops):
-            school_rtree.insert(i, (lon, lat, lon, lat))
-        mapping[school] = (school_stops, school_rtree)
+        mapping[school] = list(stops[school].items())
     return mapping
 
-def school_stops_to_routes(grid, sch_to_stoplist_rtree, buses, max_dist_miles, max_stops):
+def closest(q, ps):
+    (c, d) = (ps[0], geopy.distance.vincenty(q, ps[0][0]).miles)
+    for p in ps:
+        di = geopy.distance.vincenty(q, p[0]).miles
+        if di < d:
+            (c, d) = (p, di)
+    return (c, [p for p in ps if p != c])
+
+def school_stops_to_routes(grid, file_students, sch_to_stoplist, buses, max_dist_miles, max_stops):
     routes_by_school = {}
     routes = []
-    bus_index = 0
-    for (sch, (stops, rtree)) in tqdm(list(sch_to_stoplist_rtree.items()), desc='Generating routes'):
+    school_stop_to_bus = {}
+    bus_index = -1
+    for (sch, stops) in tqdm(list(sch_to_stoplist.items()), desc='Generating routes'):
+
+        # Build R-tree of stops for this school.
+        stops_rtree = rtree.index.Index()
+        for (i, ((lon, lat), count)) in enumerate(stops):
+            stops_rtree.insert(i, (lon, lat, lon, lat))
+
+        # Start a new route with a new bus.
+        bus_index += 1
+        if bus_index >= len(buses):
+            print('Not enough buses.')
+            exit()
         bus = buses[bus_index]
         route = Route(grid, (bus['Bus Longitude'], bus['Bus Latitude']), bus['Bus ID'])
-        while True:
-            stop_index = next(rtree.nearest(route.end(), 1), None)
-            if stop_index is None:
+
+        # Assign a bus route to every stop until none are left.
+        while True: # len(stops) > 0
+            # (((lon, lat), load), stops) = closest(route.end(), stops)
+            stop_index = next(stops_rtree.nearest(route.end(), 1), None)
+            if stop_index is None: # No more stops remaining.
                 break
             ((lon, lat), load) = stops[stop_index]
+            stops_rtree.delete(stop_index, (lon, lat, lon, lat))
+            
+            # Add the stop to the current route.
             reached_stop = route.stop((lon, lat), load)
-            if not reached_stop and\
-               (lon, lat) != (-71.10632060507754, 42.345238669042814): # Individual location cannot be reached in segment graph.
+            school_stop_to_bus[(sch, (lon, lat))] = bus['Bus ID'] # Record in order to update student records.
+            if not reached_stop: # Individual location cannot be reached in segment graph.
                 print("Could not reach " + str((lon, lat)) + ".")
-            rtree.delete(stop_index, (lon, lat, lon, lat))
 
-            # If the route is becoming too long, finish it and start a new one.
-            if route.distance > max_dist_miles or\
-               len(route.stops) > max_stops or\
-               route.load >= bus['Bus Capacity']:
+            # If there are still stops remaining but the route is becoming too long,
+            # finish it and start a new one.
+            if len(stops) > 0 and\
+               ( route.distance >= max_dist_miles or\
+                 len(route.stops) >= max_stops or\
+                 route.load >= bus['Bus Capacity'] ):
                 # Add school and record the route.
                 route.stop(sch)
                 routes_by_school.setdefault(sch, []).append(route)
@@ -92,22 +122,31 @@ def school_stops_to_routes(grid, sch_to_stoplist_rtree, buses, max_dist_miles, m
                 # Start a new route with a new bus.
                 bus_index += 1
                 if bus_index >= len(buses):
-                    break
-                
+                    print('Not enough buses.')
+                    exit()
                 bus = buses[bus_index]
                 route = Route(grid, (bus['Bus Longitude'], bus['Bus Latitude']), bus['Bus ID'])
 
-        # Leave the outer for loop if there are no more buses.
-        if bus_index >= len(buses):
-            break
+        # We exited the loop, so finish off the last (still under construction) route.
+        route.stop(sch)
+        routes_by_school.setdefault(sch, []).append(route)
+        routes.append(route)
+
+    # Update the student data with the bus assigned to each student.
+    students = geojson.load(open(file_students, 'r'))
+    for f in tqdm(students.features, desc='Updating student data with bus assignments'):
+        coords = f.geometry.coordinates
+        f['properties']['bus_id'] = school_stop_to_bus[(tuple(coords[2]), tuple(coords[1]))]
+    open(file_students, 'w').write(geojson.dumps(students, indent=2))
 
     return routes
 
 if __name__ == "__main__":
     grid = Grid('input/segments-prepared.geojson')
     buses = json.load(open('output/buses.json', 'r'))
-    stops = stops_to_dict('output/stops.json')
-    routes = school_stops_to_routes(grid, school_to_stops_rtree(stops), buses, max_dist_miles=10, max_stops=20)
+    students = geojson.load(open('output/students.geojson', 'r'))
+    stops = stops_to_dict('output/stops.json')    
+    routes = school_stops_to_routes(grid, 'output/students.geojson', school_to_stops(stops), buses, max_dist_miles=20, max_stops=40)
     open('output/routes.geojson', 'w').write(geojson.dumps(geojson.FeatureCollection([f for r in routes for f in r.features()])))
     open('output/routes.html', 'w').write(geoleaflet.html(geojson.FeatureCollection([f for r in routes for f in r.features()])))
 
